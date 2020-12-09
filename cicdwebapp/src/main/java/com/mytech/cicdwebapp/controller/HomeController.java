365ecc1720028110f6acfdd92bbae2584fed3d37
Brajesh Pandey <pandey.brajesh9718@gmail.com>
2020-12-09 14:00:35 +0530

    Add GetMethod

--- cicdwebapp/src/main/java/com/mytech/cicdwebapp/controller/HomeController.java
+++ cicdwebapp/src/main/java/com/mytech/cicdwebapp/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,5 +15,10 @@ public class HomeController {
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
+	}
+	
+	@GetMapping("sayHello")
+	public String sayHello() {
+		return "Hi Team This Is CICDAPP";
 	}
 }
