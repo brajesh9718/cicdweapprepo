a41f07387a4291a0d494defffecc0e9769da6b64
Brajesh Pandey <pandey.brajesh9718@gmail.com>
2020-12-09 14:48:03 +0530

    Add Calc Method

--- cicdwebapp/src/main/java/com/mytech/cicdwebapp/controller/HomeController.java
+++ cicdwebapp/src/main/java/com/mytech/cicdwebapp/controller/HomeController.java
@@ -6,9 +6,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.mytech.cicdwebapp.service.Calculator;
 
 @Controller
 public class HomeController {
@@ -23,4 +26,24 @@ public class HomeController {
 	public String sayHello() {
 		return "Hi Team This Is CICDAPP....";
 	}
+	
+	@GetMapping(value = "/getFactorial/{no}")
+	@ResponseBody
+	public int getFactorial(@PathVariable int no) {
+		Calculator cal = (x) -> {
+			int fact =1;
+			if(x==0)
+				return 1;
+			else if(x>0) {
+				for (int i = 1; i <= x;i++) {
+					fact = fact * i;
+				}
+				return fact;
+			}
+			return fact;
+			
+		};		
+		return cal.getFactorial(no);
+	}
+	
 }
---
+++ cicdwebapp/src/main/java/com/mytech/cicdwebapp/service/Calculator.java
@@ -0,0 +1,8 @@
+package com.mytech.cicdwebapp.service;
+
+@FunctionalInterface
+public interface Calculator {
+	
+	int getFactorial(int no);
+
+}
