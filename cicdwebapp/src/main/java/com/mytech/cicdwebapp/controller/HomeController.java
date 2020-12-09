61acbb300978309bafde249907dab54eb33bff52
Brajesh Pandey <pandey.brajesh9718@gmail.com>
2020-12-09 14:03:53 +0530

    Response

--- cicdwebapp/src/main/java/com/mytech/cicdwebapp/controller/HomeController.java
+++ cicdwebapp/src/main/java/com/mytech/cicdwebapp/controller/HomeController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,6 +19,7 @@ public class HomeController {
 	}
 	
 	@GetMapping("sayHello")
+	@ResponseBody
 	public String sayHello() {
 		return "Hi Team This Is CICDAPP";
 	}
